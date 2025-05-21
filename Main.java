acafa41f9de3c7ff27619b3f3d90f62668b97654
gokulgopi31 <sanaru2000@gmail.com>
2025-05-21 14:50:09 +0530

    loop

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int array[]={1,2,3,4,5};
+        for(int i:array){
+            System.out.println(i);
+        }
     }
 }
